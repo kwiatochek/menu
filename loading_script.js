a26880136a6844e15c82a0e8292efef6ebc042a0
kwiatochek <kvatkovskijvsevolodk@gmail.com>
2026-04-14 15:50:27 +0300

    first commit

---
+++ loading_script.js
@@ -0,0 +1,3 @@
+window.addEventListener("load" , function(){
+    document.querySelector(".loading-container").classList.add("loaded");
+})
---
+++ nav_panel_script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("navSidepanel").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("navSidepanel").style.width = "0";
+}
